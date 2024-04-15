187c2479aab66bb8745cb33471cc20293724238d
Hareesh Sreenivasan <trivandrum50@gmail.com>
2024-04-15 23:18:10 +0530

    first commit

---
+++ src/main/java/az/uk/allianz/rapgitdemo/RapGitDemoApplication.java
@@ -0,0 +1,13 @@
+package az.uk.allianz.rapgitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapGitDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/uk/allianz/rapgitdemo/RapGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package az.uk.allianz.rapgitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
